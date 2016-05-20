ad7f712a6de8dd729425f5c10896273ba18dee98
Anirban Acharjee <anirban_acharjee@m2633320lg8wp.symc.symantec.com>
2016-05-20 16:58:01 -0700

    starting merge sort and all subsets

---
+++ divide-and-conquer/MergeSort.java
@@ -0,0 +1,5 @@
+class MergrSort {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ misc/AllSubsets.java
@@ -0,0 +1,8 @@
+class AllSubsets {
+	public static ArrayList<ArrayList<Integer>> getAllSubsets() {
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
